fd95fdae0c60abe16c67cf09e7b60d45a49ca414
kmzdev <clyzivane7@hotmail.com>
2026-09-25 17:49:12 +0200

    Update train_tinystories.py

--- config/train_tinystories.py
+++ config/train_tinystories.py
@@ -1,4 +1,5 @@
-out_dir = 'out-tinystories'
+out_dir = 'out-tinystories-v2'
+init_from = 'scratch'
 eval_interval = 1000
 eval_iters = 100
 log_interval = 50
@@ -7,19 +8,19 @@ wandb_log = False
 
 dataset = 'tinystories'
 gradient_accumulation_steps = 1
-batch_size = 32
+batch_size = 128
 block_size = 256
 
-n_layer = 4
+n_layer = 6
 n_head = 4
 n_embd = 256
-dropout = 0.1                
+dropout = 0.1
 bias = False
 
 learning_rate = 6e-4
-max_iters = 50000
-lr_decay_iters = 50000
+max_iters = 100000
+lr_decay_iters = 100000
 min_lr = 6e-5
 beta2 = 0.95
-warmup_iters = 500
+warmup_iters = 1000
 weight_decay = 1e-1
